836c47878a2cb0523536820fe1d0bec87bed9531
lijin <lijin@goclouds.cn>
2022-03-19 18:49:59 +0800

    hello jenkins

---
+++ demo/src/main/java/com/example/demo/controller/HelloControllerer.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Copyright 2021 goclouds. All Rights Reserved.
+ *
+ * @Author LiJin
+ * @Date 2022/3/19
+ * @Description
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloControllerer {
+
+
+    @GetMapping("/jenkins")
+    public String hello()
+    {
+        return "hello";
+    }
+}
