1ef97d0aac1a6c706c34aeb4c557919dd30b8f20
lijin <lijin@goclouds.cn>
2022-03-20 13:14:14 +0800

    continue test autodeploy springboot

--- demo/stop.sh
+++ demo/stop.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-jar_name='HelloPipline'
+jar_name='demo-0.0.1-SNAPSHOT.jar'
 echo "Stopping" ${jar_name}
-pid= `ps -ef | grep ${jar_name} | awk '{print $2}'`
+pid=`ps -ef |grep ${jar_name} | grep -v grep | awk '{print $2}'`
+echo "查询到的Pid为" ${pid}
 if [ -n "$pid" ]
 then
 echo "kill -9 的pid:" $pid
-kill - 9 $pid
+kill -9 $pid
 fi
+
+
+
