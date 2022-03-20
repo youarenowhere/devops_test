202494b0b6f414f44ac414c03163562b86e09549
lijin <lijin@goclouds.cn>
2022-03-20 09:03:35 +0800

    增加关闭进程的sh

---
+++ demo/stop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+jar_name='HelloPipline'
+echo "Stopping" ${jar_name}
+pid= `ps -ef | grep ${jar_name} | grep -v grep | awk '{print $2}'`
+if [ -n "$pid" ]
+then
+echo "kill -9 的pid:" $pid
+kill - 9 $pid
+fi
