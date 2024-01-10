af306fa16db35962fd3c449407c7fc439ad2b5fd
lee <iamnowhere@vip.qq.com>
2024-01-10 19:28:39 +0800

    修改pom

--- demo/src/main/java/com/example/demo/controller/HelloControllerer.java
+++ demo/src/main/java/com/example/demo/controller/HelloControllerer.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description
  */
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/doc")
 public class HelloControllerer {
 
 
